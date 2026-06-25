1b4d8e2805e31c336e0a9216fd07670e1a15c161
httpsnicotic <https.nicotic.io@gmail.com>
2026-06-24 22:50:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-tsParticles.load("tsparticles", {
-  particles: {
-    number: {
-      value: 80
-    },
-    color: {
-      value: "#ffffff"
-    },
-    links: {
-      enable: true,
-      color: "#7a2cff",
-      distance: 150
-    },
-    move: {
-      enable: true,
-      speed: 2
-    },
-    size: {
-      value: 2
-    }
-  }
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
+    window.location.href = "portal.html";
 });
