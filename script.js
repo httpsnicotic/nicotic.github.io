5f166f6cd01f53e2d65b81ddf31d793800cd6e1c
httpsnicotic <https.nicotic.io@gmail.com>
2026-06-24 22:04:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// NICOTIC JS
+tsParticles.load("tsparticles", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#ffffff"
+    },
+    links: {
+      enable: true,
+      color: "#7a2cff",
+      distance: 150
+    },
+    move: {
+      enable: true,
+      speed: 2
+    },
+    size: {
+      value: 2
+    }
+  }
+});
