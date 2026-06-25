6ccb33a741e154eeeaf204112050e7141c04532d
httpsnicotic <https.nicotic.io@gmail.com>
2026-06-24 23:09:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    await tsParticles.load("tsparticles", {
+        fullScreen: { enable: true },
+        background: {
+            color: "#000"
+        },
+        particles: {
+            number: {
+                value: 60
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: "circle"
+            },
+            opacity: {
+                value: 0.5
+            },
+            size: {
+                value: 3
+            },
+            move: {
+                enable: true,
+                speed: 2
+            }
+        }
+    });
+
     const boton = document.getElementById("enterBtn");
 
     if (boton) {
